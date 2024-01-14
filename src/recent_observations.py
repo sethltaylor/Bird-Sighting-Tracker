774694b80885a001249b2c6d2a560a03697ae0b4
sethltaylor <setht1191@gmail.com>
2024-01-14 18:27:53 -0500

    Minor formatting changes
    Small changes based on linting check

--- src/recent_observations.py
+++ src/recent_observations.py
@@ -2,13 +2,17 @@ import os
 import requests
 
 def api_setup():
+
     """Function to set up headers for api call"""
+
     api_key = os.environ.get("EBIRD_API_KEY")
     headers = {'X-eBirdApiToken': api_key}
 
     return headers
 
 def get_recent_obs():
+
+    """Function to get the most recent observations for the US going back 30 days"""
     headers = api_setup()
 
     url = 'https://api.ebird.org/v2/data/obs/US/recent'
--- test/test_api.py
+++ test/test_api.py
@@ -1,7 +1,7 @@
 import os
 import requests
 import pytest
-from src.recent_observations import api_setup, get_recent_obs
+from src.recent_observations import api_setup
 
 @pytest.fixture
 def api_key():
@@ -15,4 +15,5 @@ def test_api_setup(api_key):
 
 def test_api_connection():
     response = requests.get('https://api.ebird.org/v2/data/obs/US/recent', headers = api_setup())
+
     assert response.status_code == 200
