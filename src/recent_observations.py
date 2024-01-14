9a00e165ae52891ce8f818f8baf2bfdee1a2f2f6
sethltaylor <setht1191@gmail.com>
2024-01-14 18:16:10 -0500

    Creating tests for api call function

--- src/recent_observations.py
+++ src/recent_observations.py
@@ -3,7 +3,7 @@ import requests
 
 def api_setup():
     """Function to set up headers for api call"""
-    api_key = os.getenv("EBIRD_API_KEY")
+    api_key = os.environ.get("EBIRD_API_KEY")
     headers = {'X-eBirdApiToken': api_key}
 
     return headers
@@ -14,10 +14,7 @@ def get_recent_obs():
     url = 'https://api.ebird.org/v2/data/obs/US/recent'
     params = {'back': 30} #Specifies getting data 30 days back
     response = requests.get(url, headers = headers, params = params)
-  
     return response.json()
     
-    
-
 if __name__ == "__main__":
     get_recent_obs()
---
+++ test/test_api.py
@@ -0,0 +1,18 @@
+import os
+import requests
+import pytest
+from src.recent_observations import api_setup, get_recent_obs
+
+@pytest.fixture
+def api_key():
+    return os.environ.get('EBIRD_API_KEY')
+
+def test_api_setup(api_key):
+    headers = api_setup()
+
+    assert 'X-eBirdApiToken' in headers
+    assert headers['X-eBirdApiToken'] == api_key
+
+def test_api_connection():
+    response = requests.get('https://api.ebird.org/v2/data/obs/US/recent', headers = api_setup())
+    assert response.status_code == 200
