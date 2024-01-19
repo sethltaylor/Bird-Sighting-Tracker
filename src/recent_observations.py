651fb9cce74635374c23ee9dd38c5a6267378efe
sethltaylor <setht1191@gmail.com>
2024-01-18 23:41:09 -0500

    Initial function to batch write to DDB
    Next: Function to update items in DDB

--- src/recent_observations.py
+++ src/recent_observations.py
@@ -1,5 +1,6 @@
 import os
 import requests
+import boto3
 
 def api_setup():
 
@@ -10,15 +11,46 @@ def api_setup():
 
     return headers
 
-def get_recent_obs():
+def get_recent_obs(days: int) -> list:
 
-    """Function to get the most recent observations for the US going back 30 days"""
+    """Function to get the most recent observations for the US going back a number of specified days"""
     headers = api_setup()
 
     url = 'https://api.ebird.org/v2/data/obs/US/recent'
-    params = {'back': 30} #Specifies getting data 30 days back
+    params = {'back': days} #Specifies number of days to retrieve data for
     response = requests.get(url, headers = headers, params = params)
-    return response.json()
-    
-if __name__ == "__main__":
-    get_recent_obs()
+    data = response.json()
+    return data
+
+def connect_to_table():
+    dynamodb = boto3.resource('dynamodb') #boto3 should check env for access keys automatically
+    table_name = 'RecentObservations'
+    table = dynamodb.Table(table_name)
+    return table
+
+def batch_write_data(data: list, table) -> None:
+    with table.batch_writer() as writer:
+        for item in data:
+            writer.put_item(
+                Item={
+                'speciesCode': item['speciesCode'],
+                'obsDt': item['obsDt'],
+                'comName': item['comName'],
+                'sciName': item['sciName'],
+                'locId': item['locId'],
+                'locName': item['locName'],
+                'howMany': item['howMany'], 
+                'lat': str(item['lat']),  # Lat/Long converted to string because DDB doesn't handle floats
+                'lng': str(item['lng']),  
+                'obsValid': item['obsValid'],
+                'obsReviewed': item['obsReviewed'],
+                'locationPrivate': item['locationPrivate'],
+                'subId': item['subId'],
+                }
+                )
+
+#if __name__ == "__main__":
+    #data = get_recent_obs()
+    #table = connect_to_table()
+    #batch_write_data(data, table)
+
