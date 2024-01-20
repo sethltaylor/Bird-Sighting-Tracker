ab0f4806e5f52f42828409d1872227b562395369
sethltaylor <setht1191@gmail.com>
2024-01-20 16:50:58 -0500

    Adding ttl functoin
    Adding a function to add a time to live attribute to data before writing to DDB

--- src/recent_observations.py
+++ src/recent_observations.py
@@ -1,6 +1,7 @@
 import os
 import requests
 import boto3
+import time
 import concurrent.futures
 
 def api_setup():
@@ -51,6 +52,17 @@ def get_recent_obs(days: int, region_codes: list) -> list:
        
         return data
 
+def add_ttl(data: list) -> list:
+    """This function adds a time to live attribute to each observation based on the observation date + 30 days"""
+    data_with_ttl = []
+    for entry in data:
+        obs_timestamp = time.mktime(time.strptime(entry['obsDt'], "%Y-%m-%d %H:%M"))
+        ttl_timestamp = int(obs_timestamp + (30*24*60*60)) #Add 30 days in seconds. TTL is in epoch time. 30 days * 24 hours * 60 minutes * 60 seconds
+        entry['ttl'] = ttl_timestamp
+
+        data_with_ttl.append(entry)
+    
+    return data_with_ttl
 
 def connect_to_table():
     """Function to establish connection to the DDB table"""
@@ -62,7 +74,7 @@ def connect_to_table():
 def batch_write_obs(data: list, table) -> None:
     """Function to batch write items to the DDB table, which is more efficient from a read/write perspective than just put_item"""
 
-    keys_to_include = ['speciesCode', 'comName', 'sciName', 'locId', 'locName', 'obsDt', 'howMany', 'lat', 'lng', 'subId']
+    keys_to_include = ['speciesCode', 'comName', 'sciName', 'locId', 'locName', 'obsDt', 'howMany', 'lat', 'lng', 'subId', 'ttl']
 
     with table.batch_writer() as writer:
         for item in data:
@@ -82,24 +94,24 @@ def batch_write_obs(data: list, table) -> None:
         
         print(f"Wrote {len(data)} items to table.")
 
-def update_recent_obs(data: list, table) -> None:
-    keys_to_include = ['speciesCode', 'comName', 'sciName', 'locId', 'locName', 'obsDt', 'howMany', 'lat', 'lng', 'subId']
+# def update_recent_obs(data: list, table) -> None:
+#     keys_to_include = ['speciesCode', 'comName', 'sciName', 'locId', 'locName', 'obsDt', 'howMany', 'lat', 'lng', 'subId']
     
-    for item in data:
-        dynamo_item = {}
-        for key in keys_to_include:
-            if key in item:
-                    # Convert lat and lng to strings
-                value = str(item[key]) if key in ['lat', 'lng'] else item[key]
-                dynamo_item[key] = value
+#     for item in data:
+#         dynamo_item = {}
+#         for key in keys_to_include:
+#             if key in item:
+#                     # Convert lat and lng to strings
+#                 value = str(item[key]) if key in ['lat', 'lng'] else item[key]
+#                 dynamo_item[key] = value
 
     
-        table.update_item(
-            Key ={
-                'subId': item['subId'],
-                'speciesCode': item['speciesCode'] #Not complete. Read boto3 doc
-            }
-        )
+#         table.update_item(
+#             Key ={
+#                 'subId': item['subId'],
+#                 'speciesCode': item['speciesCode'] #Not complete. Read boto3 doc
+#             }
+#         )
 
 if __name__ == "__main__":
     regions = get_regions('subnational2', ['US-VA','US-MD','US-DC'])
