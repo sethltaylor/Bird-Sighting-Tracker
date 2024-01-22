f0a1e207e1abfaaebbec71c5f773c108c3c52e84
sethltaylor <setht1191@gmail.com>
2024-01-22 13:58:37 -0500

    Adding iam policy for lambda
    Adding iam policies to give lambda function access to parameter store + writing to dyanmoDB

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -9,7 +9,32 @@ data "aws_iam_policy_document" "lambda_assume_role_policy"{
   }
 }
 
+data "aws_iam_policy_document" "lambda_execution_policy"{
+  statement {
+    effect = "Allow"
+    actions = ["ssm:GetParameter"]
+    resources = ["arn:aws:ssm:us-east-1:851725613770:parameter/EBIRD_API_KEY"]
+  }
+
+  statement {
+    effect = "Allow"
+    actions = ["dynamodb:BatchWriteItem"]
+    resources = [aws_dynamodb_table.recent_observation_table.arn]
+  }
+}
+
 resource "aws_iam_role" "lambda_execution_role" {  
   name = "lambda_execution_role"  
   assume_role_policy = data.aws_iam_policy_document.lambda_assume_role_policy.json
+}
+
+resource "aws_iam_policy" "lambda_execution_policy" {  
+  name = "lambda_execution_policy"  
+  description= "Permissions for parameters store and writing to dynamodb"
+  policy = data.aws_iam_policy_document.lambda_execution_policy.json
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_policy_attachment" {
+  role = aws_iam_role.lambda_execution_role.name
+  policy_arn = aws_iam_policy.lambda_execution_policy.arn
 }
