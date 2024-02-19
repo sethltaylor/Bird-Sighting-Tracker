6636b008d13c51dcba55833efa39b319c3d6c3d1
sethltaylor <setht1191@gmail.com>
2024-02-19 13:55:51 -0500

    Handling errors with dates

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,11 +3,11 @@ import pandas as pd
 import boto3
 import pydeck as pdk
 from datetime import datetime
-from decimal import Decimal
 
 st.set_page_config(layout="wide", page_title = "Bird Sightings in DC, Maryland, and Virginia", page_icon= ":bird:")
 
-def query_bird_sightings(common_name, start_date, end_date):
+#Functions to get data
+def query_bird_sightings(common_name_selection, start_date_str, end_date_str):
     dynamodb = boto3.resource('dynamodb')
     table_name = 'RecentObservations'
     table = dynamodb.Table(table_name)
@@ -35,73 +35,98 @@ def fetch_common_names(bucket_name, file_name):
     return common_names
 
 #Streamlit UI
-st.title('Bird Sightings in the DMV Area')
 
-common_names = fetch_common_names('recent-observations-keys', 'recent_observation_keys.csv')
+#Add sidebar for data filter selection
+with st.sidebar:
+    st.sidebar.title('Inputs for Mapping')
+    common_names = fetch_common_names('recent-observations-keys', 'recent_observation_keys.csv')
 
-common_name_selection = st.selectbox('Select a bird', options = common_names['comName'].unique())
-start_date_selection = st.date_input('Start Date for Sightings Search', value = pd.to_datetime('today') - pd.Timedelta(days=30))
-st.caption('Data is available for the past 30 days.')
-end_date_selection = st.date_input('End Date for Sightings Search', value = pd.to_datetime('today'))
+    common_name_selection = st.selectbox('Select a bird', options = common_names['comName'].unique())
+    
+    start_date_selection = st.date_input('Start Date for Sightings Search', value = pd.to_datetime('today') - pd.Timedelta(days=30))
+    st.caption('Data is available for the past 30 days.')
+    end_date_selection = st.date_input('End Date for Sightings Search', value = pd.to_datetime('today'))
 
-#Convert dates to strings to support query
-start_date_str = start_date_selection.strftime('%Y-%m-%d')
-end_date_str = end_date_selection.strftime('%Y-%m-%d')
+    #Check if end date is after start date. 
+    if end_date_selection < start_date_selection:
+        st.error('Error: End date must be after start date')
 
-#Map sightings
-if st.button('Show Sightings'):
-    filtered_df = query_bird_sightings(common_name_selection, start_date_str, end_date_str)
+    #Setting end date to be the latest value in the day and start date to be the earliest time in the day to support intra-day querying. 
+    start_datetime = datetime.combine(start_date_selection, datetime.min.time())
+    end_datetime = datetime.combine(end_date_selection, datetime.max.time())
 
-    if not filtered_df.empty:
-        #Perform data cleaning/converstion
-        filtered_df = filtered_df.astype({'lng':'float', 'lat':'float'})
-        filtered_df['howMany'] = pd.to_numeric(filtered_df['howMany'], errors = 'coerce')
-        filtered_df['howMany'].fillna(1, inplace= True)
+    start_date_str = start_datetime.strftime('%Y-%m-%d %H:%M')
+    end_date_str = end_datetime.strftime('%Y-%m-%d %H:%M')
 
-        #Create mapping dictionary 
-        data_dict = filtered_df[['lat', 'lng', 'locName', 'howMany']].to_dict(orient='records')
+#Add tabs
 
-        # Pydeck map
-        layer = pdk.Layer(
-            "ScatterplotLayer",
-            data_dict,
-            get_position="[lng, lat]",
-            get_color="[200, 30, 0, 160]",
-            get_radius=2000,
-            pickable=True,
-        )
+tab1, tab2 = st.tabs(["Bird Sightings", "About"])
 
-        view_state = pdk.ViewState(
-            latitude= filtered_df['lat'].mean(),
-            longitude= filtered_df['lng'].mean(),
-            zoom=6,
-            pitch=0,
-        )
+with tab1:
+    st.title('Bird Sightings in the DMV Area')
 
-        deck = pdk.Deck(
-            layers=[layer],
-            initial_view_state=view_state,
-            map_style='mapbox://styles/mapbox/light-v10',
-            tooltip = {
-                "html": "<b>Location:</b> {locName} <br/> <b>Number of Birds Sighted:</b> {howMany}",
-                "style": {
-                    "backgroundColor": "steelblue",
-                    "color": "white"
-                    }
-                }
+    #Map sightings
+    if st.button('Show Sightings'):
+        filtered_df = query_bird_sightings(common_name_selection, start_date_str, end_date_str)
+
+        if not filtered_df.empty:
+            #Perform data cleaning/converstion
+            filtered_df = filtered_df.astype({'lng':'float', 'lat':'float'})
+            filtered_df['howMany'] = pd.to_numeric(filtered_df['howMany'], errors = 'coerce')
+            filtered_df['howMany'].fillna(1, inplace= True)
+
+            #Create mapping dictionary 
+            data_dict = filtered_df[['lat', 'lng', 'locName', 'howMany']].to_dict(orient='records')
+
+            # Pydeck map
+            layer = pdk.Layer(
+                "ScatterplotLayer",
+                data_dict,
+                get_position="[lng, lat]",
+                get_color="[200, 30, 0, 160]",
+                get_radius=2000,
+                pickable=True,
             )
 
-        st.pydeck_chart(deck)
-    
-        #Calculate average number of sightings
-        st.subheader(f"Total number of {common_name_selection} sighted between {start_date_str} and {end_date_str}")
-        st.write(round(filtered_df['howMany'].sum()))
+            view_state = pdk.ViewState(
+                latitude= filtered_df['lat'].mean(),
+                longitude= filtered_df['lng'].mean(),
+                zoom=6,
+                pitch=0,
+            )
 
-        st.dataframe(filtered_df)
+            deck = pdk.Deck(
+                layers=[layer],
+                initial_view_state=view_state,
+                map_style='mapbox://styles/mapbox/light-v10',
+                tooltip = {
+                    "html": "<b>Location:</b> {locName} <br/> <b>Number of Birds Sighted:</b> {howMany}",
+                    "style": {
+                        "backgroundColor": "steelblue",
+                        "color": "white"
+                        }
+                    }
+                )
 
-    else:
-        st.write(f"No {common_name_selection} were sighted between {start_date_str} and {end_date_str}.")
+            st.pydeck_chart(deck)
+        
+            #Calculate average number of sightings
+            st.subheader(f"Total number of {common_name_selection} sighted between {start_date_str} and {end_date_str}")
+            st.write(round(filtered_df['howMany'].sum()))
 
+            st.dataframe(filtered_df)
 
+        else:
+            st.write(f"No {common_name_selection} were sighted between {start_date_str} and {end_date_str}.")
 
-
+with tab2:
+    st.title("About")
+    st.write("This app is developed by Seth Taylor.")
+    st.markdown("""
+        <a href="https://www.linkedin.com/in/seth-taylor-3106486b/" target="_blank">
+            <img src="https://content.linkedin.com/content/dam/me/business/en-us/amp/brand-site/v2/bg/LI-Bug.svg.original.svg" width="30" height="30" style="margin-right: 10px;">LinkedIn Profile
+        </a><br>
+        <a href="https://github.com/sethltaylor" target="_blank">
+            <img src="https://raw.githubusercontent.com/FortAwesome/Font-Awesome/6.x/svgs/brands/github.svg" width="30" height="30" style="margin-right: 10px;">GitHub Profile
+        </a>
+        """, unsafe_allow_html=True)
