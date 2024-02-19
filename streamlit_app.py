683b7ab61e8ecf7065b40b8b1e54a9cb23f09b71
sethltaylor <setht1191@gmail.com>
2024-02-19 11:47:47 -0500

    Initial mapping of data points

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,7 @@ import pandas as pd
 import boto3
 import pydeck as pdk
 from datetime import datetime
+from decimal import Decimal
 
 st.set_page_config(layout="wide", page_title = "Bird Sightings in DC, Maryland, and Virginia", page_icon= ":bird:")
 
@@ -14,9 +15,9 @@ def query_bird_sightings(common_name, start_date, end_date):
     response = table.query(
         IndexName = 'comName-obsDt-index',
         Select = 'ALL_ATTRIBUTES',
-        KeyConditionExpression = "comName = :comName AND obsDt BETWEEN :start_date AND :end_date",
+        KeyConditionExpression = "comName = :common_name AND obsDt BETWEEN :start_date AND :end_date",
         ExpressionAttributeValues = {
-            ':comName': common_name_selection,
+            ':common_name': common_name_selection,
             ':start_date': start_date_str, 
             ':end_date': end_date_str,
         }
@@ -47,6 +48,52 @@ end_date_selection = st.date_input('End Date for Sightings Search', value = pd.t
 start_date_str = start_date_selection.strftime('%Y-%m-%d')
 end_date_str = end_date_selection.strftime('%Y-%m-%d')
 
+#Map sightings
 if st.button('Show Sightings'):
     filtered_df = query_bird_sightings(common_name_selection, start_date_str, end_date_str)
-    st.write(filtered_df)
+
+    #Perform data cleaning/converstion
+    filtered_df = filtered_df.astype({'lng':'float', 'lat':'float'})
+    filtered_df['howMany'] = pd.to_numeric(filtered_df['howMany'], errors = 'coerce')
+    filtered_df['howMany'].fillna(1, inplace= True)
+
+    if not filtered_df.empty:
+        data_dict = filtered_df[['lat', 'lng', 'locName', 'howMany']].to_dict(orient='records')
+
+        # Pydeck map
+        layer = pdk.Layer(
+            "ScatterplotLayer",
+            data_dict,
+            get_position="[lng, lat]",
+            get_color="[200, 30, 0, 160]",
+            get_radius=2000,
+            pickable=True,
+        )
+
+        view_state = pdk.ViewState(
+            latitude= filtered_df['lat'].mean(),
+            longitude= filtered_df['lng'].mean(),
+            zoom=6,
+            pitch=0,
+        )
+
+        deck = pdk.Deck(
+            layers=[layer],
+            initial_view_state=view_state,
+            map_style='mapbox://styles/mapbox/light-v10',
+            tooltip = {
+                "html": "<b>Location:</b> {locName} <br/> <b>Number of Birds Sighted:</b> {howMany}",
+                "style": {
+                    "backgroundColor": "steelblue",
+                    "color": "white"
+   }
+}
+        )
+
+        st.pydeck_chart(deck)
+    
+    #Calculate average number of sightings
+    st.subheader(f"Total number of {common_name_selection} sighted between {start_date_str} and {end_date_str}")
+    st.write(round(filtered_df['howMany'].sum()))
+
+    st.dataframe(filtered_df)
