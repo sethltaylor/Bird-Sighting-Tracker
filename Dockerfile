42fdb2c8c57c50696bef6f25583fa6349a001211
sethltaylor <setht1191@gmail.com>
2024-02-25 13:09:46 -0500

    Correct security group rules + docker healthcheck
    From port 8501 to port 8501 required in security group and dockerfile health check needed curl to register healthy status.

--- Dockerfile
+++ Dockerfile
@@ -5,7 +5,7 @@ WORKDIR /app
 COPY ./requirements.txt /app/requirements.txt
 COPY ./streamlit_app.py /app/streamlit_app.py
 
-RUN pip3 install -r /app/requirements.txt
+RUN apt-get update && apt-get install -y curl && pip3 install -r /app/requirements.txt
 
 EXPOSE 8501
 
--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -60,6 +60,13 @@ resource "aws_security_group" "bird_tracker_sg_tf" {
         cidr_blocks = ["0.0.0.0/0"]
     }
 
+    ingress {
+        from_port = 8501
+        to_port = 8501
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
     egress {
         from_port        = 0
         to_port          = 0
