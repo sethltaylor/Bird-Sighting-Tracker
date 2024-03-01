4094057888e92c400af3a737fe6aae5d23f4336d
sethltaylor <setht1191@gmail.com>
2024-02-29 21:17:10 -0500

    Removing expontential backoff
    Error in exponential backoff resulted in writing to the table repeatedly within the same lambda execution.

--- terraform/dynamodb.tf
+++ terraform/dynamodb.tf
@@ -1,12 +1,10 @@
 resource "aws_dynamodb_table" "recent_observation_table" {
     name = "RecentObservations"
     billing_mode = "PROVISIONED"
-    read_capacity = 5
-    write_capacity = 15
+    read_capacity = var.recent_observation_table_read_capacity
+    write_capacity = var.recent_observation_table_write_capacity
     hash_key = "subId"
-    range_key = "speciesCode"
-
- 
+    range_key = "comName"
 
     attribute{
         name = "subId"
@@ -14,32 +12,12 @@ resource "aws_dynamodb_table" "recent_observation_table" {
     }
 
     attribute {
-        name = "speciesCode"
-        type = "S"
-    }
-
-    attribute {
       name = "comName"
-      type = "S"
-    }
-
-    attribute {
-      name = "obsDt"
       type = "S"
     }
     
     ttl {
         attribute_name = "ttl"
         enabled = true 
-    }
-
-    global_secondary_index {
-      name = "comName-obsDt-index"
-      hash_key = "comName"
-      range_key = "obsDt"
-      projection_type = "INCLUDE"
-      non_key_attributes = ["howMany", "lat", "lng","locName"]
-      read_capacity = 20
-      write_capacity = 10
     }
 }
