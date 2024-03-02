a10712eda114b9903c36a871d781ead7cc19e6d5
sethltaylor <setht1191@gmail.com>
2024-03-02 13:13:37 -0500

    Set up HTTPS listener + SG rule

--- terraform/loadbalancer.tf
+++ terraform/loadbalancer.tf
@@ -23,3 +23,16 @@ resource "aws_lb_listener" "listener" {
       target_group_arn = aws_alb_target_group.target_group.arn
     }
 }
+
+resource "aws_lb_listener" "https_listener" {
+    load_balancer_arn = aws_alb.ecs_alb.arn
+    port              = 443
+    protocol          = "HTTPS"
+    ssl_policy        = "ELBSecurityPolicy-TLS13-1-2-2021-06"  
+    certificate_arn   = "arn:aws:acm:us-east-1:851725613770:certificate/9750229d-1433-4930-bf4c-7c71b6ba7cd7"
+
+    default_action {
+        type             = "forward"
+        target_group_arn = aws_alb_target_group.target_group.arn
+    }
+}
